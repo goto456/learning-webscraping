2a993fadc35706abea4394b0b0284c5cf6458e3c
wangbiwen [王必文] <wangbiwen@xiaomi.com>
2016-12-09 21:55:38 +0800

    delete some code.

--- builtwith_test.py
+++
@@ -1,5 +0,0 @@
-
-import builtwith
-
-result = builtwith.parse('http://www.toutiao.com')
-print result
--- whois_test.py
+++
@@ -1,6 +0,0 @@
-
-import whois
-
-print whois.whois('www.goto456.com')
-# print whois.whois('appspot.com')
-
